dba31d16a2133402c01e330abb9c8f80973206a4
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-03 04:41:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -24,13 +24,7 @@ class script(object):
     ADMIN_TXT = HELP_TXT
 
     CUSTOM_FILE_CAPTION = (
-        "<strong><blockquote>{filename}\n\n"
-        "Mᴏʀᴇ Mᴏᴠɪᴇꜱ Jᴏɪɴ @sandalwood_kannada_moviesz\n\n"
-        "Tᴇᴀᴍ : @KR_Picture\n\n"
-        "Uᴘʟᴏᴀᴅᴇᴅ Bʏ 👉\n"
-        "https://t.me/+X5CwwZB-jV9iODc1\n"
-        "https://t.me/+X5CwwZB-jV9iODc1"
-        "</blockquote></strong>"
+        "<strong><blockquote>{filename}\n\nMᴏʀᴇ Mᴏᴠɪᴇꜱ Jᴏɪɴ @sandalwood_kannada_moviesz\n\nTᴇᴀᴍ : @KR_Picture\n\nUᴘʟᴏᴀᴅᴇᴅ Bʏ 👉\nhttps://t.me/+X5CwwZB-jV9iODc1\nhttps://t.me/+X5CwwZB-jV9iODc1</blockquote></strong>"
     )
 
     STATUS_TXT = """<b><u>Cᴜʀʀᴇɴᴛ Dᴀᴛᴀʙᴀsᴇ Sᴛᴀᴛᴜs</b></u>
