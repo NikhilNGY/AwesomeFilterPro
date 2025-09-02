4404d07b814f4fc4afcdc6692ed64c9edb616314
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-03 02:15:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -799,7 +799,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("I couldn't find any movie in that name.")
+        k = await msg.reply("I Cᴏᴜʟᴅɴ'ᴛ Fɪɴᴅ Aɴʏ Mᴏᴠɪᴇ, Mᴏᴠɪᴇ RᴇQᴜᴇꜱᴛ Aᴄᴄᴇᴘᴛᴇᴅ, Mᴏᴠɪᴇ Aᴠᴀɪʟᴀʙʟᴇ Sᴏᴏɴ.")
         await asyncio.sleep(8)
         await k.delete()
         return
@@ -828,7 +828,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        k = await msg.reply("I Cᴏᴜʟᴅɴ'ᴛ Fɪɴᴅ Aɴʏ Mᴏᴠɪᴇ, Mᴏᴠɪᴇ RᴇQᴜᴇꜱᴛ Aᴄᴄᴇᴘᴛᴇᴅ, Mᴏᴠɪᴇ Aᴠᴀɪʟᴀʙʟᴇ Sᴏᴏɴ.")
         await asyncio.sleep(8)
         await k.delete()
         return
