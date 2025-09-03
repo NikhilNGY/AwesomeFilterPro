6cc7b57a76d9cbeb60dac7a3aaccc39b83a1c6a4
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-03 10:47:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -365,47 +365,48 @@ async def cb_handler(client: Client, query: CallbackQuery):
             alert = alerts[int(i)]
             alert = alert.replace("\\n", "\n").replace("\\t", "\t")
             await query.answer(alert, show_alert=True)
- if query.data.startswith("file"):
-     ident, file_id = query.data.split("#")
-     files_ = await get_file_details(file_id)
-     if not files_:
-         return await query.answer('ɴᴏ sᴜᴄʜ ғɪʟᴇ ᴇxɪᴛs.')
+        if query.data.startswith("file"):
+            ident, file_id = query.data.split("#")
+            files_ = await get_file_details(file_id)
+            if not files_:
+                return await query.answer('ɴᴏ sᴜᴄʜ ғɪʟᴇ ᴇxɪᴛs.')
 
-     files = files_[0]
-     title = files.file_name
-     size = get_size(files.file_size)
-     f_caption = files.caption
-     settings = await get_settings(query.message.chat.id)
+           files = files_[0]
+           title = files.file_name
+           size = get_size(files.file_size)
+           f_caption = files.caption 
+           settings = await 
+       get_settings(query.message.chat.id)
 
-    if CUSTOM_FILE_CAPTION:
-        try:
-            f_caption = CUSTOM_FILE_CAPTION.format(
-                filename='' if title is None else title,
-                file_size='' if size is None else size,
-                file_caption='' if f_caption is None else f_caption
-            )
-        except Exception as e:
-            logger.exception(e)
+      if CUSTOM_FILE_CAPTION:
+          try:
+              f_caption = CUSTOM_FILE_CAPTION.format(
+                  filename='' if title is None else title,
+                  file_size='' if size is None else size,
+                  file_caption='' if f_caption is None else f_caption
+             )
+         except Exception as e:
+             logger.exception(e)
+ 
+     if f_caption is None:
+         f_caption = title or "File"
 
-    if f_caption is None:
-        f_caption = title or "File"
-
-    try:
-        if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-            return
-        elif settings.get('botpm'):
-            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-            return
-        else:
-            await client.send_cached_media(
-                chat_id=query.from_user.id,
-                file_id=file_id,
-                caption=f_caption,
-                protect_content=(ident == "filep")
-            )
-    except Exception as e:
-        logger.exception(e)
+     try:
+         if AUTH_CHANNEL and not await is_subscribed(client, query):
+             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+             return
+         elif settings.get('botpm'):
+             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+             return
+         else:
+             await client.send_cached_media(
+                 chat_id=query.from_user.id,
+                 file_id=file_id,
+                 caption=f_caption,
+                 protect_content=(ident == "filep")
+             )
+     except Exception as e:
+         logger.exception(e)
                 await query.answer('ᴄʜᴇᴄᴋ ᴘᴍ ɪ ʜᴀᴠᴇ sᴇɴᴅ ғɪʟᴇs', show_alert=True)
         except UserIsBlocked:
             await query.answer('ᴜɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀɴʜ !', show_alert=True)
